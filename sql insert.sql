009f6953a864e9246d76c4a95aa8f5f164f10489
unknown <ruanx14@gmail.com>
2020-08-04 16:29:50 -0300

    view de pagina help pronta, e index quase pronta

---
+++ sql insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`, `rateDrop`, `wiki`) VALUES ('helm', 'A Frogzard ate my head', 'Greenguard Challenge', '0.02', 'http://aq-3d.wikidot.com/a-frogzard-ate-my-head');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('helm', 'Necrohen hat', 'Skulltower');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`, `rateDrop`, `wiki`) VALUES ('helm', 'UWU Shrade Mask', 'Camp Gonnagetcha', '', 'http://aq-3d.wikidot.com/uwu-shrade-mask');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('helm', 'Vampiric Visage', 'Darkovia Waterway');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('helm', 'Yettun Head', 'Frostvale Dungeons');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('helm', 'Dr Trollestein\'s Head', 'Lab');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`, `wiki`) VALUES ('weapon', 'Soulborne Axe', 'Skulltower', 'http://aq-3d.wikidot.com/soulborne-axe');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`, `wiki`) VALUES ('weapon', 'Midas\' Curse', 'Dreadfool\'s Labyrint', 'http://aq-3d.wikidot.com/midas-curse');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('weapon', 'King\'s Ransom', 'Doomwood');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('weapon', 'Doom Ripper', 'Bone Cliff');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('weapon', 'The Headless Horseman\'s Sword', 'Mogloween');
+INSERT INTO `checkuprare`.`item` (`typeItem`, `name`, `locale`) VALUES ('weapon', 'Legion Soulbrand', 'Arena of Souls');
